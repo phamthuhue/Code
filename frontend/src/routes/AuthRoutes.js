84a6841bac7927940c4ff5e6bac260c2829348a3
phamhue <thuhue3503@gmail.com>
2025-06-11 00:49:48 +0700

    add changepassword path, remove search in homepage

--- frontend/src/routes/AuthRoutes.js
+++ frontend/src/routes/AuthRoutes.js
@@ -6,6 +6,8 @@ import { Login } from "pages/auth/Login";
 import { ForgotPassword } from "pages/auth/ForgotPassword";
 import { ResetPassword } from "pages/auth/ResetPassword";
 import { Register } from "pages/auth/Register";
+import { ChangePassword } from "pages/auth/ChangePassword";
+
 export const AuthRoutes = () => (
   // Các route auth bên dưới là các route không cần bảo vệ
   // luôn bắt đầu bằng /auth
@@ -14,6 +16,7 @@ export const AuthRoutes = () => (
     <Route index element={<Login />} />
     <Route path="login" element={<Login />} />
     <Route path="register" element={<Register />} />
+    <Route path="change-password" element={<ChangePassword />} />
     <Route path="forgot-password" element={<ForgotPassword />} />
     <Route path="reset-password/:token" element={<ResetPassword />} />
   </>
