8169b25d3a1e835c9c80fab6e4a43163b6f9f234
phamhue <thuhue3503@gmail.com>
2025-06-11 00:13:45 +0700

    update itinerary router

--- admin/src/routes.js
+++ admin/src/routes.js
@@ -3,13 +3,14 @@ import React from 'react'
 const Dashboard = React.lazy(() => import('./views/dashboard/Dashboard'))
 const Tour = React.lazy(() => import('./views/tour/Tour'))
 const Booking = React.lazy(() => import('./views/booking/Booking'))
+const Itinerary = React.lazy(() => import('./views/itinerary/Itinerary'))
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
   { path: '/dashboard', name: 'Dashboard', element: Dashboard },
   { path: '/tour', name: 'Tour', element: Tour },
-  { path: '/booking', name: 'Booking', element: Booking },
-  { path: '/itinerary', name: 'Tour', element: Itinerary },
+  { path: '/booking', name: 'Danh sách phiếu đặt', element: Booking },
+  { path: '/itinerary', name: 'Lịch trình', element: Itinerary },
 ]
 
 export default routes
