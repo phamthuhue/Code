51e109ae28b9da679509393a41426765b0f5cb91
phamhue <thuhue3503@gmail.com>
2025-06-18 21:30:24 +0700

    delete header of api service

--- admin/src/services/Api/bookingDetailService.js
+++ admin/src/services/Api/bookingDetailService.js
@@ -1,17 +1,6 @@
 import axiosConfig from '../axiosConfig'
 const API = '/booking-details'
 export const getBookingDetail = (id) => axiosConfig.get(`${API}/${id}`);
-export const createBookingDetail = (data) =>
-  axiosConfig.post(API, data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  })
-
-export const updateBookingDetail = (id, data) =>
-  axiosConfig.put(`${API}/${id}`, data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  })
+export const createBookingDetail = (data) => axiosConfig.post(API, data)
+export const updateBookingDetail = (id, data) => axiosConfig.put(`${API}/${id}`, data)
 export const deleteBookingDetail = (id) => axiosConfig.delete(`${API}/${id}`)
--- admin/src/services/Api/bookingService.js
+++ admin/src/services/Api/bookingService.js
@@ -1,19 +1,9 @@
 import axiosConfig from '../axiosConfig'
 const API = '/bookings'
 export const getBookings = () => axiosConfig.get(API)
-export const createBooking = (data) =>
-  axiosConfig.post(API, data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  })
+export const createBooking = (data) => axiosConfig.post(API, data)
 
-export const updateBooking = (id, data) =>
-  axiosConfig.put(`${API}/${id}`, data, {
-    headers: {
-      'Content-Type': 'multipart/form-data',
-    },
-  })
+export const updateBooking = (id, data) => axiosConfig.put(`${API}/${id}`, data)
 export const confirmMultipleBookings = (bookingIds) => {
   return axiosConfig.put(`${API}/confirm-multiple`, {
     bookingIds: bookingIds
